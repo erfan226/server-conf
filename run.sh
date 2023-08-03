6e877d2d2f466753b6c633713ec0bce836f132bf
Erfan <Erfan226@gmail.com>
2023-08-03 18:36:47 +0330

    Update run.sh
    Fix permission to write in ssh config

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo adduser carl --shell=/bin/false --no-create-home
 sudo ufw allow 9200/tcp
-sed -i '16i\Port 9200' /etc/ssh/sshd_config
+sudo sed -i '16i\Port 9200' /etc/ssh/sshd_config
 sudo service ssh restart
 sudo apt update
 sudo apt install nginx
