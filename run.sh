6e1d03925e7b96baf0021033623cd32698afb51b
Erfan <erfan@Macs-MacBook-Pro.local>
2023-08-14 13:15:26 +0330

    Add new port &  pip package

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-sudo adduser carl --shell=/bin/false --no-create-home
 sudo ufw allow 9200/tcp
+sudo ufw allow 55642/tcp
 sudo sed -i '16i\Port 9200' /etc/ssh/sshd_config
+sudo sed -i '16i\Port 55642' /etc/ssh/sshd_config
 sudo service ssh restart
 sudo apt update
 sudo apt install nginx
+sudo apt install python3-pip
 sudo ufw allow 'Nginx HTTP'
+sudo adduser carl --shell=/bin/false --no-create-home
